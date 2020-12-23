5440207c08a1007fb376bc73919461e315e982ce
Zak Ayesh <>
2020-12-23 11:26:42 -0500

    Upload to GitHub

---
+++ inbox/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const mnemonic = 'Example String';
+
+const provider = new HDWalletProvider(
+     mnemonic,
+    'https://rinkeby.infura.io/v3/183938d4eb184360af056f3394ba4e43'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result =  await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0] });
+
+    console.log('Contract deployed', result.options.address)
+};
+deploy();
---
+++ lottery/contracts/Lottery.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+    address public lastWinner;
+
+    constructor() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .0000000001 ether);
+
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint256) {
+        return uint256(keccak256(block.difficulty, now, players));
+    }
+
+    function pickWinner() public restricted {
+        uint256 index = random() % players.length;
+        lastWinner = players[index];
+        players[index].transfer(this.balance);
+        players = new address[](0);
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+}
